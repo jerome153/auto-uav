301ee9213e68df82f6b47fe32fca09bc85505f39
jerome153 <56744637+jerome153@users.noreply.github.com>
2025-04-25 11:52:09 -0700

    Add files via upload

---
+++ save_images/save_images_calculations.py
@@ -0,0 +1,30 @@
+
+
+from common.video.camera_coordinate_transformer import CameraCoordinateTransformer
+from common.video.video_capture import VideoCapture
+from common.video.josh_calculations import VideoCapture
+import sys
+
+import cv2
+
+
+
+# Set the ID of the marker you want to capture
+TARGET_MARKER_ID = 24
+
+
+print("Grayscale image saved as marker_capture.png")
+
+
+
+def get_marker_frame(target_id):
+
+    print(TARGET_MARKER_ID)
+    
+           
+
+
+
+
+print("Grayscale image saved as marker_capture.png")
+
